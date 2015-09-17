e7f86a936b01668b129aca678596c024d8d17cb1
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:23:51 -0700

    Initial commit

---
+++ test/dot-cache-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var dot-cache = require(__dirname + '/lighter-dot-cache')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/dot-cache-test.js
@@ -0,0 +1,26 @@
+var dot-cache = require('../lighter-dot-cache')
+var is = global.is || require('exam/lib/is')
+
+describe('dot-cache', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
